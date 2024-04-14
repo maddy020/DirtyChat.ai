4c921016f3c0673cc50ca0f886896d9614f5798e
Madhav Setia <madhavsetia24@gmail.com>
2024-04-14 21:17:18 +0530

    Adding posthog and changing supabase config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ToastContainer } from "react-toastify";
+import { CSPostHogProvider } from "./provider";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <CSPostHogProvider>
+        <Component {...pageProps} />
+      </CSPostHogProvider>
       <ToastContainer
         position="top-right"
         autoClose={2500}
---
+++ src/pages/provider.tsx
@@ -0,0 +1,14 @@
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+const key = process.env.NEXT_PUBLIC_POSTHOG_KEY as string;
+const host = process.env.NEXT_PUBLIC_POSTHOG_HOST as string;
+
+if (typeof window !== "undefined") {
+  posthog.init(key, {
+    api_host: host,
+  });
+}
+export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
